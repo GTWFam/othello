db004e0c4904d57c0aa17bf42423b384f9968430
GTWFam <252704436go@gmail.com>
2022-06-10 18:17:27 -0400

    Bug fixes

--- src/public/scripts/googleAnalytics.js
+++ src/public/scripts/googleAnalytics.js
@@ -1,21 +1,16 @@
+window.dataLayer = window.dataLayer || [];
 export function gtag() {
   dataLayer.push(arguments);
 }
 
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    fetch(`/getGACode`, { method: "get", "no-cors": true })
-      .then((res) => res.json())
-      .then((data) => {
-        gtag("js", new Date());
+fetch(`/getGACode`, { method: "get", "no-cors": true })
+  .then((res) => res.json())
+  .then((data) => {
+    gtag("js", new Date());
 
-        gtag("config", data.GA_CODE, {
-          siteSpeedSampleRate: 100,
-          cookieFlags: "SameSite=None; Secure",
-          allowLinker: true,
-        });
-      });
-  },
-  false
-);
+    gtag("config", data.GA_CODE, {
+      siteSpeedSampleRate: 100,
+      cookieFlags: "SameSite=None; Secure",
+      allowLinker: true,
+    });
+  });
